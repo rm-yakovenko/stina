6ca6c0768a7f0bc6d9bc4f6c77638921032abcef
Oleksii30 <polyanovskyyol@gmail.com>
2021-10-08 19:36:22 +0300

    Updates

---
+++ static/js/main.a37c1b2c.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpstina=this.webpackJsonpstina||[]).push([[0],{9:function(t,n,e){"use strict";e.r(n);var c=e(1),i=e.n(c),r=e(3),s=e.n(r),o=e(0);var a=function(){return Object(o.jsx)(o.Fragment,{children:"Hello stina"})},u=function(t){t&&t instanceof Function&&e.e(3).then(e.bind(null,10)).then((function(n){var e=n.getCLS,c=n.getFID,i=n.getFCP,r=n.getLCP,s=n.getTTFB;e(t),c(t),i(t),r(t),s(t)}))};s.a.render(Object(o.jsx)(i.a.StrictMode,{children:Object(o.jsx)(a,{})}),document.getElementById("root")),u()}},[[9,1,2]]]);
+//# sourceMappingURL=main.a37c1b2c.chunk.js.map
